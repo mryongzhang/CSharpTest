5e4ad6700be2a10166d932051f97435f8d8d5a4c
mryongzhang <yongzhang@163.com>
2013-07-16 19:46:23 +0900

    单机程序测试Petapoco微型ORM

---
+++ TestPetapoco/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DefaultConnection;
+
+//////////////////////////////////////////////////////////////////////////
+// 测试Petapoco，连接AdventureWorks库
+// 两种方式：1，标准的通过自动生成的表对象进行访问
+//          2, 通过dynamic方式访问（无需事先定义跟表对应的类）
+namespace TestPetapoco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (var db = new DefaultConnectionDB())
+            {
+                IEnumerable<Address> adds = db.Query<Address>("select top 1000 * from Person.Address");
+
+                foreach (var a in adds)
+                {
+                    Console.WriteLine(a.City);
+                }
+
+                long count = db.ExecuteScalar<long>("select count(*) from Person.Address");
+                Console.WriteLine("count:{0}", count);
+
+                db.Dispose();
+            }
+
+            using (var db = new DefaultConnectionDB())
+            {
+                IEnumerable<dynamic> adds = db.Query<dynamic>("select top 1000 * from Person.Address");
+                foreach (var a in adds)
+                {
+                    Console.WriteLine(a.City);
+                }
+                db.Dispose();                
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
