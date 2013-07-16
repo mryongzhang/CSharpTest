bf8c32030fafdf917f3a7e468ac9079bf7cf9911
mryongzhang <yongzhang@163.com>
2013-07-17 00:09:38 +0800

    silverlight和javascript交互

---
+++ SLandJS/SLandJS/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Windows.Browser;
+
+namespace SLandJS
+{
+    [ScriptableType()]
+    public partial class MainPage : UserControl
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            HtmlPage.RegisterScriptableObject("page", this);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ScriptObject scriptObject = (ScriptObject)HtmlPage.Window.GetProperty("createText");
+            scriptObject.InvokeSelf("Jerry", "Hello World");
+        }
+
+        [ScriptableMember]
+        public void ChangeText(string newText)
+        {
+            txbTest.Text = "It's invoking by Javascript " + newText;
+        }
+    }
+}
---
+++ SLandJS/readme.md
@@ -0,0 +1,5 @@
+#silverlight和javascript交互
+
+silverlight和javascript交互的示例程序
+
+参考地址：http://www.cnblogs.com/HellenTian/archive/2010/11/11/1874711.html
